7a89e2882ab4a8e227f93f798cd028ca2ec479e3
Dmitry Buravtsov <dmitry@MacBook-Pro-Dmitriy.local>
2019-06-22 00:35:46 +0300

    getting json from local server #task6

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import s from './style.scss';
+import {CommonData} from "Root/containers/App";
 
-class App extends React.Component {
+class App extends React.Component<CommonData> {
     render() {
-
+        const {tickets} = this.props;
+        console.log(tickets);
         return <div>
             <h1 className={s.Title}>Starter component</h1>
         </div>
---
+++ src/containers/App.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import App from 'Components/App';
+
+const fetchTickets = () => fetch('http://localhost:3000/tickets', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+    .then(res => {
+        if (res.status === 200) {
+            return res.json();
+        } else {
+            throw new Error(res.statusText);
+        }
+    });
+
+export interface CommonData {
+    tickets: any,
+    error: boolean,
+}
+
+class AppContainer extends React.Component<object, CommonData> {
+    state = {
+        tickets: {},
+        error: false,
+    };
+
+    componentDidMount() {
+        this.getTickets();
+    }
+
+    async getTickets() {
+        try {
+            const tickets = await fetchTickets();
+            this.setState({
+                tickets
+            })
+        } catch (e) {
+            this.setState({
+                error: true,
+            })
+        }
+    }
+
+    render() {
+        const {tickets, error} = this.state;
+        return <App tickets={tickets} error={error}/>;
+    }
+}
+
+export default AppContainer;
